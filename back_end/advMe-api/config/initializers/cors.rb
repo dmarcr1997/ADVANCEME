fb55220e356c6e991469bc4122d8f62fd352aef2
dmarcr1997 <dmarcr1997@gmail.com>
2020-07-15 15:39:56 -0500

    changed cors

--- back_end/advMe-api/config/initializers/cors.rb
+++ back_end/advMe-api/config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://advance-4f4a0.firebaseapp.com/'
 
-    resource '*',
+    resource 'http://localhost:3006/',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
